d3be154b3de499cb943016fa91d67a377f052e95
Shaikh Atif <141632543+ShaikhAtif01@users.noreply.github.com>
2025-10-18 19:12:19 +0530

    Update Header.jsx
    header logo has been updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -124,7 +124,7 @@ const Header = () => {
           <div className="collapse navbar-collapse w-100 justify-content-between align-items-center mt-3 mt-lg-0 d-none d-lg-flex">
             <Link className="navbar-brand" to="/">
               <img
-                src="src/assets/logo.svg"
+                src="https://www.sigmaguys.com/uploads/logo/logo.svg"
                 alt="logo"
                 width="100"
                 height="50"
